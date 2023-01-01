566ef5d3b392f3a7536799c851f406e958a325d1
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-12-31 17:22:59 -0800

    remove some garble

--- commands/simple-english.js
+++ commands/simple-english.js
@@ -1,9 +1,8 @@
 const { SlashCommandBuilder } = require('discord.js');
 const Database = require("easy-json-database")
-const devMode = typeof __E_IS_DEV !== "undefined" && __E_IS_DEV;
-const n = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/database/n.json`)
-const v = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/database/v.json`)
-const d = new Database(`${devMode ? S4D_NATIVE_GET_PATH : "."}/database/d.json`)
+const n = new Database('./database/n.json')
+const v = new Database('./database/v.json')
+const d = new Database('./database/d.json')
 
 function mathRandomInt(a, b) {
 	return Math.floor(Math.random() * (b - a + 1) + a);
