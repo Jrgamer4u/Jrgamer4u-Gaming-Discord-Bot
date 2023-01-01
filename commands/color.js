15741dbc889915215ab57e3d10d81bb9279d9aab
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2022-12-31 17:16:07 -0800

    add

---
+++ commands/color.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+function colourRandom() {
+	var num = Math.floor(Math.random() * Math.pow(2, 24));
+	return '#' + ('00000' + num.toString(16)).slice(-6);
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('color')
+		.setDescription('gives random color number'),
+	async execute(interaction) {
+		return interaction.reply(colourRandom());
+	},
+};
---
+++ commands/hi.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('hi')
+		.setDescription('says nah'),
+	async execute(interaction) {
+		return interaction.reply('nah');
+	},
+};
