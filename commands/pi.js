41c83626598590c3c6c9303c76757ac761e56ea3
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2023-01-02 20:02:13 -0800

    gjccg

--- commands/pi.js
+++ commands/pi.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("pi")
-    .setDescription("says 15 digits of pi"),
-  async execute(interaction) {
-    return interaction.reply(`${Math.PI}`);
-  },
+	data: new SlashCommandBuilder()
+		.setName("pi")
+		.setDescription("says 15 digits of pi"),
+	async execute(interaction) {
+		return interaction.reply(`${Math.PI}`);
+	},
 };
--- commands/version.js
+++ commands/version.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("version")
-    .setDescription("shows version"),
-  async execute(interaction) {
-    return interaction.reply("OBR Second MileStone");
-  },
+	data: new SlashCommandBuilder()
+		.setName("version")
+		.setDescription("shows version"),
+	async execute(interaction) {
+		return interaction.reply("OBR Second MileStone");
+	},
 };
