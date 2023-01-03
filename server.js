1fba59bae6f854eaae6fdc9afd9304229cbc80c6
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2023-01-03 15:53:19 -0800

    add dockerfile

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ server.get("/", function (_req, res) {
 	res.send("Your bot is alive!");
 });
 function keepAlive() {
-	server.listen(3000, () => {
-		console.log("Server is Ready!");
+	const port = 3000;
+	server.listen(port, () => {
+		console.log(`Server is Ready, at http://localhost:${port}`);
 	});
 }
 
