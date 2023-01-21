0c0bd5df1b1d898d2b4ab6f4ca5d0598b6af4b29
Jrgamer4u <66277204+Jrgamer4u@users.noreply.github.com>
2023-01-20 20:08:47 -0800

    3 to 1 json file

--- commands/simple-english.js
+++ commands/simple-english.js
@@ -1,8 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const fs = require("node:fs");
-const n = JSON.parse(fs.readFileSync("./database/n.json", "utf8"));
-const v = JSON.parse(fs.readFileSync("./database/v.json", "utf8"));
-const d = JSON.parse(fs.readFileSync("./database/d.json", "utf8"));
+const simple = JSON.parse(fs.readFileSync("./database/simple.json", "utf8"));
 
 function mathRandomInt(a, b) {
 	return Math.floor(Math.random() * (b - a + 1) + a);
@@ -14,8 +12,8 @@ module.exports = {
 		.setDescription("says a sentence in simple english"),
 	async execute(interaction) {
 		return interaction.reply(
-			`${n[mathRandomInt(1, 600)]} ${v[mathRandomInt(1, 17)]} ${d[mathRandomInt(1, 17)]
-			} ${n[mathRandomInt(1, 600)]}`
+			`${simple.n[mathRandomInt(1, 600)]} ${simple.v[mathRandomInt(1, 17)]} ${simple.d[mathRandomInt(1, 17)]
+			} ${simple.n[mathRandomInt(1, 600)]}`
 		);
 	},
 };
